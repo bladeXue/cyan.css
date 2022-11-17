4af43ae672239f580748a41c419455eb09a5d867
Z1\bladexue <blade1565@outlook.com>
2022-11-17 22:18:12 +0800

    update

--- angular/ng-pale-template/src/app/app.module.ts
+++ angular/ng-pale-template/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BtnComponent } from './btn/btn.component';
+// import { BtnCoreComponent } from './btn-core/btn-core.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BtnComponent,
+    // BtnCoreComponent
   ],
   imports: [
     BrowserModule,
